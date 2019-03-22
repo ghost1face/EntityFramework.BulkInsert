764f9d0dd09dde4982c14c62a12be1443452853f
icnocop <icnocop@users.noreply.github.com>
2019-03-22 00:09:25 -0700

    Added database with default values test

---
+++ src/EntityFramework.BulkInsert.Test/CodeFirst/BulkInsert/SqlServer/DatabaseWithDefaultValues.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace EntityFramework.BulkInsert.Test.CodeFirst.BulkInsert.SqlServer
+{
+    [DbConfigurationType(typeof(SqlContextConfig))]
+    public class DatabaseWithDefaultValues : DbContext
+    {
+        public virtual IDbSet<Audit> Audits { get; set; }
+
+        public DatabaseWithDefaultValues()
+            : base("TestContext")
+        {
+            Database.SetInitializer<DatabaseWithDefaultValues>(new DatabaseWithDefaultValuesInitializer());
+        }
+
+        private class DatabaseWithDefaultValuesInitializer : IDatabaseInitializer<DatabaseWithDefaultValues>
+        {
+            public void InitializeDatabase(DatabaseWithDefaultValues context)
+            {
+                context.Database.ExecuteSqlCommand(
+                    "ALTER TABLE [dbo].[Audits] ADD CONSTRAINT [DF_Audit_AuditUserName] DEFAULT (suser_sname()) FOR [AuditUserName];");
+            }
+        }
+    }
+
+    public class Audit
+    {
+        [Key]
+        public long AuditId { get; set; }
+
+        [Required]
+        public string AuditUserName { get; set; }
+    }
+}
---
+++ src/EntityFramework.BulkInsert.Test/CodeFirst/BulkInsert/SqlServer/DatabaseWithDefaultValuesTest.cs
@@ -0,0 +1,25 @@
+﻿using EntityFramework.BulkInsert.Extensions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace EntityFramework.BulkInsert.Test.CodeFirst.BulkInsert.SqlServer
+{
+    [TestFixture]
+    public class DatabaseWithDefaultValuesTest
+    {
+        [Test]
+        public void BulkInsert_WithDefaultValues_InsertsDefaultValues()
+        {
+            using (DatabaseWithDefaultValues ctx = new DatabaseWithDefaultValues())
+            {
+                ctx.Database.CreateIfNotExists();
+
+                ctx.Database.Initialize(false);
+
+                List<Audit> audits = new List<Audit> { new Audit() };
+
+                ctx.BulkInsert(audits);
+            }
+        }
+    }
+}
